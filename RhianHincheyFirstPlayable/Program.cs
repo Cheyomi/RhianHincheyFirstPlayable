a3349d485c69892782d0ac3d5e125cd2aa147fc6
Rhian <139021661+Cheyomi@users.noreply.github.com>
2025-11-17 10:50:38 -0400

    Initial commit

---
+++ RhianHincheyFirstPlayable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+
+class Test
+{
+    public static void Main()
+    {
+        string path = @"RPGMap.txt"; //Getting the file as a string
+        string[] map = File.ReadAllLines(path, Encoding.UTF8); //Reading all lines of the file into an array
+
+        foreach (string line in map)
+        {
+            Console.WriteLine(line);
+        }
+
+    }
+}

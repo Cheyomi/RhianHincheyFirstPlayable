82859a9703c0002b493ce0145c65d7ba8a31d6e0
Rhian <139021661+Cheyomi@users.noreply.github.com>
2025-11-17 12:14:10 -0400

    Added map
    Colored, designed map

--- RhianHincheyFirstPlayable/Program.cs
+++ RhianHincheyFirstPlayable/Program.cs
@@ -5,15 +5,67 @@ using System.Text;
 
 class Test
 {
+
+    static string path = @"RPGMap.txt";
+    static string[] map = File.ReadAllLines(path, Encoding.UTF8); //Reading all lines of the file into an array
     public static void Main()
     {
-        string path = @"RPGMap.txt"; //Getting the file as a string
-        string[] map = File.ReadAllLines(path, Encoding.UTF8); //Reading all lines of the file into an array
 
-        foreach (string line in map)
-        {
-            Console.WriteLine(line);
-        }
+        DisplayMap();
+
+        //▒ - Grass (Majority)
+        //▓ - Mountain
+        //░ - Forest
+
 
     }
+
+    static void DisplayMap()
+    {
+        foreach (string line in map)
+        {
+            foreach (char tile in line)
+            {
+                if (tile == '▒')
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                }
+                else if (tile == '▓')
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else if (tile == '░')
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                }
+                else if (tile == '+')
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else if (tile == '-')
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else if (tile == '|')
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+
+                    Console.Write(tile);
+                
+
+            }
+
+            Console.WriteLine();
+            Console.ResetColor();
+
+
+        }
+
+
+
+
+    }
+
+
 }
